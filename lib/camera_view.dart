a8d8ee61506b31e39ffca07caf2c56c1c941ae47
Kolade <kolademodupe007@gmail.com>
2025-01-23 12:23:05 +0100

    update 1

--- lib/camera_view.dart
+++ lib/camera_view.dart
@@ -1,6 +1,22 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
 
 class CameraView extends StatefulWidget {
+  final String title;
+  final CustomPaint? customPaint;
+  final String? text;
+  final Function(InputImage inputImage)? onImage;
+  final CameraLensDirection initialDirection;
+
+  const CameraView({
+    super.key,
+    this.title = 'Camera View',
+    this.customPaint,
+    this.text,
+    required this.onImage,
+    required this.initialDirection ,
+  });
 
   @override
   State<CameraView> createState() => _CameraViewState();
